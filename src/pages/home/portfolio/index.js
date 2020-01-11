4ac53eaeee279cb6174d16f8724b76ce94257dc6
Julian Blagis <julianbgs96@gmail.com>
2020-01-11 20:06:30 +0200

    finish desktop version portfolio

--- src/pages/home/portfolio/index.js
+++ src/pages/home/portfolio/index.js
@@ -1,9 +1,15 @@
 import './index.sass';
 import Swiper from 'swiper/js/swiper.min';
 import 'swiper/css/swiper.min.css';
+import 'magnific-popup/dist/jquery.magnific-popup.min';
+import 'magnific-popup/dist/magnific-popup.css';
 
 export default () => {
   const $elemSlide = $('.portfolio__img');
+  const $nextArr = $('.portfolio__next');
+  const $prevArr = $('.portfolio__prev');
+  const $nextBg = $('.portfolio__next-bg');
+  const $prevBg = $('.portfolio__prev-bg ');
   const portfolioSwiper = new Swiper('.portfolio__slider', {
       slidesPerView: 'auto',
       spaceBetween: 20,
@@ -29,10 +35,43 @@ export default () => {
     });
   }
 
+  function overAnimationBg(elem) {
+    elem.addClass('portfolio__show-bg');
+    elem.removeClass('portfolio__hide-bg');
+  }
+
+  function outAnimationBg(elem) {
+    elem.addClass('portfolio__hide-bg');
+    elem.removeClass('portfolio__show-bg');
+  }
+
+  function openPopup(elem) {
+    elem.magnificPopup({
+      type: 'image'
+    });
+  }
+
   // zoom animation
   $elemSlide.each((index, elem) => {
     overAnimationZoom($(elem));
     outAnimationZoom($(elem));
+    openPopup($(elem));
   });
+  // end
+
+  // add bg on click to arrows
+    $nextArr.on('click', function () {
+      overAnimationBg($nextBg);
+      setTimeout(() => {
+        outAnimationBg($nextBg);
+      }, 1500)
+    });
+
+  $prevArr.on('click', function () {
+      overAnimationBg($prevBg);
+      setTimeout(() => {
+        outAnimationBg($prevBg);
+      }, 1500)
+    });
   // end
 }
