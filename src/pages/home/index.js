d9d54e239da2900b4ea4d8dfa901a5a112c599ac
Julian Blagis <julianbgs96@gmail.com>
2020-01-11 17:47:42 +0200

    add portfolio section, setting slider, create logic for appearance zoom elem

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,9 @@
 import './index.sass';
 import './index.pug';
 import initMain from './main/index';
+import initPortfolio from './portfolio/index';
 
 export default () => {
   initMain();
+  initPortfolio();
 };
--- src/pages/home/portfolio/index.js
+++ src/pages/home/portfolio/index.js
@@ -1,3 +1,38 @@
 import './index.sass';
+import Swiper from 'swiper';
+import 'swiper/css/swiper.min.css';
 
-export default () => {}
+export default () => {
+  const $elemSlide = $('.portfolio__img');
+  const portfolioSwiper = new Swiper('.portfolio__slider', {
+      slidesPerView: 'auto',
+      spaceBetween: 20,
+      navigation: {
+        nextEl: '.portfolio__next',
+        prevEl: '.portfolio__prev',
+      },
+    });
+
+  function overAnimationZoom(elem) {
+    elem.mouseover(() => {
+      const $link = elem.find('.portfolio__zoom');
+      $link.addClass('portfolio__show-zoom');
+      $link.removeClass('portfolio__fade-zoom');
+    });
+  }
+
+  function outAnimationZoom(elem) {
+    elem.mouseout(() => {
+      const $link = elem.find('.portfolio__zoom');
+      $link.addClass('portfolio__fade-zoom');
+      $link.removeClass('portfolio__show-zoom');
+    });
+  }
+
+  // zoom animation
+  $elemSlide.each((index, elem) => {
+    overAnimationZoom($(elem));
+    outAnimationZoom($(elem));
+  });
+  // end
+}
