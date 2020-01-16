d14804e08297b6a332076b0fa6e3b4b9ba04a759
Julian Blagis <julianbgs96@gmail.com>
2020-01-16 23:37:40 +0200

    add more elements in section about

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,8 +2,10 @@ import './index.sass';
 import './index.pug';
 import initMain from './main/index';
 import initPortfolio from './portfolio/index';
+import initAbout from './about/index';
 
 export default () => {
   initMain();
   initPortfolio();
+  initAbout();
 };
